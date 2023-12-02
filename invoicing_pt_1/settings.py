4220cd4728275c52c59888246f9f9e8f1861c35e
ransom12 <deejha927@gmail.com>
2023-12-02 13:25:14 +0530

    updates requiremnets.txt

--- invoicing_pt_1/settings.py
+++ invoicing_pt_1/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-9!1st&8o6n--l&((eba%4gb*hdm*op0g=-r3t_9odvn1!t*6#e"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
